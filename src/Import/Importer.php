53769046c3b986402d3eef654fdcacf8bf89c1dd
dantleech <dan.t.leech@gmail.com>
2017-12-13 12:46:23 +0100

    Added some deps

---
+++ src/Import/Importer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Import;
+
+use PhpBench\Dom\Document;
+
+class Importer
+{
+    private $variantRepository;
+
+    public function import(Document $document)
+    {
+    }
+}
