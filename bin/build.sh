35460d9d3bdbeed68d1423583e2f735dde502394
dantleech <dan.t.leech@gmail.com>
2017-12-18 11:48:41 +0100

    Move composer install to travis

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-composer install
 ./bin/console doctrine:database:create --env=$ENV
 ./bin/console doctrine:schema:update --force --env=$ENV
 ./bin/console elastic:mapping:load --env=$ENV
