a5667ebe545389764ae070b9a5bedac747b45532
dantleech <dan.t.leech@gmail.com>
2017-12-21 15:36:25 +0100

    Updated importer

---
+++ src/Service/ImporterResponse.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use App\Domain\Project\ProjectName;
+
+class ImporterResponse
+{
+    /**
+     * @var ProjectName
+     */
+    private $projectName;
+
+    /**
+     * @var string
+     */
+    private $uuid;
+
+    private function __construct(ProjectName $projectName, string $uuid)
+    {
+        $this->projectName = $projectName;
+        $this->uuid = $uuid;
+    }
+
+    public static function create(ProjectName $projectName, string $uuid)
+    {
+        return new self($projectName, $uuid);
+    }
+
+    public function project(): ProjectName
+    {
+        return $this->projectName;
+    }
+
+    public function uuid(): string
+    {
+        return $this->uuid;
+    }
+}
