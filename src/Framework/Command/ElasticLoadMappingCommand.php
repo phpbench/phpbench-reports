985e095706bd519ebe91ed5e5ebc71e241b796a1
dantleech <dan.t.leech@gmail.com>
2017-12-29 13:20:38 +0100

    Added purge method

--- src/Framework/Command/ElasticLoadMappingCommand.php
+++ src/Framework/Command/ElasticLoadMappingCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Elasticsearch\Client;
 use App\Infrastructure\Elastic\MappingLoader;
+use Symfony\Component\Console\Input\InputOption;
 
 class ElasticLoadMappingCommand extends Command
 {
@@ -24,10 +25,14 @@ class ElasticLoadMappingCommand extends Command
     protected function configure()
     {
         $this->setName('elastic:mapping:load');
+        $this->addOption('purge', null, InputOption::VALUE_NONE, 'Purge all the indexes');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if ($input->getOption('purge')) {
+            $this->mappingLoader->purgeAll();
+        }
         $this->mappingLoader->loadMappings();
         $output->writeln('Done');
     }
